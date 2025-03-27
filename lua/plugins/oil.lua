60e598b4c7e2d951edf6dc70bb3a3b476a9d909d
Paul Petricevich <paul.petricevich@surfair.com>
2025-03-27 20:38:52 +1300

    updated oil settings to show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,7 +9,11 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
 }
